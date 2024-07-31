85294b9d1a0973720d2a23e141dabfb665b0d87e
Kaks256 <167288747+Kaks256@users.noreply.github.com>
2024-07-31 20:56:52 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,8 +9,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "keithkeizzah",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " keithkeizzah",              
+    OWNER_NAME: process.env.OWNER_NAME || "Google",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "256743391566",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || '𝐀𝐋𝐏𝐇𝐀-𝐌𝐃',
